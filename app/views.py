6b45cb26fd67aeae7927de508e7902852e4fc8d9
maheshreddykonda <kondamaheshreddy1999@gmail.com>
2023-06-06 20:02:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mahi(request):
+    return render(request,'mahi.html')
+def mahih2(request):
+    return render(request,'mahi2.html')
